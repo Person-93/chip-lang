0167fd37c3645dc9ebd7a2df952ccd6c4dc9cf45
person93 <person93.person93@gmail.com>
2022-03-23 13:58:05 -0400

    👷 Update safety-check.rs
    - Ignore test targets
    - Improve error message

--- src/ci/src/bin/safety-check.rs
+++ src/ci/src/bin/safety-check.rs
@@ -1,7 +1,8 @@
 #![forbid(unsafe_code)]
 
 use anyhow::Result;
-use cargo_metadata::MetadataCommand;
+use cargo_metadata::{MetadataCommand, Target};
+use console::style;
 use std::{
   fs::File,
   io::{BufRead, BufReader},
@@ -20,9 +21,7 @@ fn main() -> Result<()> {
     }
 
     for target in &package.targets {
-      if !target.kind.contains(&"bin".to_string())
-        && !target.kind.contains(&"lib".to_string())
-      {
+      if !should_check(target) {
         continue;
       }
 
@@ -31,9 +30,15 @@ fn main() -> Result<()> {
         .read_line(&mut first_line)?;
       if first_line.trim() != "#![forbid(unsafe_code)]" {
         missing_forbid += 1;
+
+        let path = target
+          .src_path
+          .strip_prefix(&metadata.workspace_root)
+          .unwrap();
         eprintln!(
-          "#![forbid(unsafe_code)] should be the first line of {}",
-          target.src_path
+          "{} should start with `{}`",
+          style(path).bright().red().bold(),
+          style("#![forbid(unsafe_code)]").bright().bold()
         );
       }
     }
@@ -41,4 +46,17 @@ fn main() -> Result<()> {
 
   // no need to take the time to run destructors
   std::process::exit(missing_forbid.clamp(0, 1));
+}
+
+fn should_check(target: &Target) -> bool {
+  let mut is_bin_or_lib = false;
+  for kind in &target.kind {
+    if kind == "test" {
+      return false;
+    }
+    if is_bin_or_lib || kind == "bin" || kind == "lib" {
+      is_bin_or_lib = true;
+    }
+  }
+  is_bin_or_lib
 }
