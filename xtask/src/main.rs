ce880fe3f8271d3bf45407d1e72d7e4c4914c0a0
person93 <person93.person93@gmail.com>
2022-03-30 03:12:31 -0400

    ⚡️ Add panic hook to xtasks

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -23,7 +23,27 @@ tasks! {
 }
 
 fn main() -> ! {
+  panic::set_hook(Box::new(panic_hook));
+
   Cli::parse().task.run()
+}
+
+fn panic_hook(info: &PanicInfo) {
+  eprint!("panicked");
+
+  if let Some(location) = info.location() {
+    eprint!(": {location}");
+  }
+
+  if let Some(message) = info.payload().downcast_ref::<&str>() {
+    eprint!(": {message}");
+  } else if let Some(message) = info.payload().downcast_ref::<String>() {
+    eprint!(": {message}");
+  }
+
+  eprintln!();
+
+  process::exit(1);
 }
 
 const PROJECT_ROOT: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../");
